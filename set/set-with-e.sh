58a8842376b0a2b02420a8ba7ced19f947222883
Sharjeel Aziz <sharjeel.aziz@gmail.com>
2024-04-05 19:09:17 -0400

    Initial commit
    Signed-off-by: Sharjeel Aziz <sharjeel.aziz@gmail.com>

---
+++ set/set-with-e.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+# script with set -e
+rm important_file
+cp backup_file important_file
---
+++ set/set-without-e.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+
+# script without set -e
+rm important_file
+cp backup_file important_file
